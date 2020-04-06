21c398ac64aaf565b0cd8731fac83112ead2def7
David Groomes <dwgroomes@gmail.com>
2020-04-06 17:25:50 -0500

    Experiment with formatting

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# java-time
+# java-time-playground
 
-Trying out different aspects of `java.time`. <https://docs.oracle.com/en/java/javase/13/docs/api/java.base/java/time/package-summary.html>
+Learning and experimenting with `java.time`. <https://docs.oracle.com/en/java/javase/13/docs/api/java.base/java/time/package-summary.html>
--- src/dgroomes/Main.java
+++ src/dgroomes/Main.java
@@ -1,33 +1,45 @@
 package dgroomes;
 
+import java.time.Instant;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
-import java.util.Objects;
 
 public class Main {
 
+    private static final Instant JAN_1_2019_INSTANT = Instant.parse("2019-01-01T12:00:00.123Z");
+    private static final ZoneId AMERICA_CHICAGO = ZoneId.of("America/Chicago");
+
     public static void main(String[] args) {
-        verifyIt("2019-01-01T12:00:00.123", DateTimeFormatter.ISO_DATE_TIME);
-        verifyIt("2019-01-01 12:00:00.123", DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss.SSS"));
+        verifyParsing("2019-01-01T12:00:00.123", DateTimeFormatter.ISO_DATE_TIME);
+        verifyParsing("2019-01-01 12:00:00.123", DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss.SSS"));
+
+        verifyFormatting(DateTimeFormatter.ISO_DATE_TIME.withZone(AMERICA_CHICAGO), "2019-01-01T06:00:00.123-06:00[America/Chicago]");
+        verifyFormatting(DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(AMERICA_CHICAGO), "2019-01-01T06:00:00.123-06:00");
     }
 
     /**
      * Try different "DateTimeFormatter"s. Trying to learn DateTimeFormatter patterns and the pre-defined options like
      * DateTimeFormatter.ISO_DATE_TIME by exercising different input datetime strings
      */
-    private static void verifyIt(String s, DateTimeFormatter f) {
+    private static void verifyParsing(String s, DateTimeFormatter f) {
         try {
             var parsed = f.parse(s);
             int found = parsed.get(ChronoField.MILLI_OF_SECOND);
             var expected = 123;
-            if (!Objects.equals(expected, found)) {
+            if (expected != found) {
                 System.err.println(String.format("Not equal! Expected %s but found %s for DateTimeFormatter %s", expected, found, f));
                 System.exit(1);
             }
         } catch (Exception e) {
-            System.err.println(String.format("Failed to parse for DateTimeFormatter '%s'. Message: %s", f, e.getMessage()));
-            System.exit(1);
+            throw new IllegalStateException(String.format("Failed to parse for DateTimeFormatter '%s'. Message: %s", f, e.getMessage()));
         }
     }
 
+    private static void verifyFormatting(DateTimeFormatter f, String expected) {
+        var found = f.format(JAN_1_2019_INSTANT);
+        if (!expected.equals(found)) {
+            throw new IllegalStateException(String.format("Not equal! Expected %s but found %s for DateTimeFormatter %s", expected, found, f));
+        }
+    }
 }
